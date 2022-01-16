406686246b9a8720d1ef52bc64d90b3977096558
Tran Trong Duc <bupbi99@gmail.com>
2022-01-16 17:20:16 +0700

    ADD: Header

---
+++ src/components/EventList/EventList.data.js
@@ -0,0 +1,26 @@
+export const mockData = [
+  {
+    date: "16/1/2022",
+    timeStart: "4:00 PM",
+    timeEnd: "4:20 PM",
+    event: "Ghé lấy máy ảnh",
+    place: "Nhà Xuân Minh",
+    remainingTime: "in 4 hr",
+  },
+  {
+    date: "17/1/2022",
+    timeStart: "4:00 PM",
+    timeEnd: "4:20 PM",
+    event: "Ghé lấy máy ảnh",
+    place: "Nhà Xuân Minh",
+    remainingTime: "in 4 hr",
+  },
+  {
+    date: "17/1/2022",
+    timeStart: "4:00 PM",
+    timeEnd: "4:20 PM",
+    event: "Ghé lấy máy ảnh",
+    place: "Nhà Xuân Minh",
+    remainingTime: "in 4 hr",
+  },
+];
