6c43dccfb9bef2671ea0a295097d3545e1095ca8
Tran Trong Duc <bupbi99@gmail.com>
2022-01-21 13:35:57 +0700

    ADD: Detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,148 @@
 import React from "react";
+import styled from "styled-components";
 
 function Detail() {
-  return <div></div>;
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://img3.thuthuatphanmem.vn/uploads/2019/10/08/anh-doctor-strange_104930908.jpg"
+          alt=""
+        />
+      </Background>
+      <ImageTitle>
+        <img
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f359881d-6bb2-4391-aba6-779f7084edd4/daa5xd3-2e62b3e3-81ef-40d9-a08c-f8d84032c544.png/v1/fill/w_1024,h_450,strp/doctor_strange___title_transparent_by_asthonx1_daa5xd3-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDUwIiwicGF0aCI6IlwvZlwvZjM1OTg4MWQtNmJiMi00MzkxLWFiYTYtNzc5ZjcwODRlZGQ0XC9kYWE1eGQzLTJlNjJiM2UzLTgxZWYtNDBkOS1hMDhjLWY4ZDg0MDMyYzU0NC5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Q4Lke3II16oZLxYDXoyBNMSiK1lDI2qs53Yx2h_TgOY"
+          alt=""
+        />
+      </ImageTitle>
+
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>2018 • 7m • Family, Fantasy, Kids, Magical</SubTitle>
+      <Description>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil ducimus
+        repellat exercitationem officiis sed? Deserunt accusantium, blanditiis
+        quia id reprehenderit magni nobis fugiat quisquam, ex, quod facere dolor
+        placeat distinctio.
+      </Description>
+    </Container>
+  );
 }
 
 export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background-color: rgb(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgb(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  font-size: 15px;
+  color: rgb(249, 249, 249);
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  color: rgb(249, 249, 249);
+  margin-top: 16px;
+`;
